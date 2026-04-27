de44fd6ee700fd417f1cfdf449963c4ca14fed97
Akshit joshi <akshitcasual1@gmail.com>
2026-04-27 20:30:58 +0000

    .

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 struct Pool {
     token0: String,
     token1: String,
-    tvl_usd: f64,
+    tvl_usd: f64, //f64 to be more precise
     fees_7d_usd: f64,
     fee_tier: String,
 }
 
 impl Pool {
-    fn apy(&self) -> f64 {
-        (self.fees_7d_usd / self.tvl_usd) * 52.0 * 100.0
+    fn apy(&self) -> f64 {  //i am borrowing data from the pool , not taking ownership
+        (self.fees_7d_usd / self.tvl_usd) * 52.0 * 100.0   //here no smeicolons because this is the return value i need
     }
+
+    /*
+    i can also write the apy funct like
+      fn apy($self) -> f64{
+      return(self.fees_7d_usd/self.tvl_usd) *52.0*100.0;
+      }
+     */
 
     fn display(&self) {
         println!("  Pair:{}/{}", self.token0, self.token1);
@@ -20,8 +27,8 @@ impl Pool {
 }
 
 fn main() {
-    let pools = vec![
-        Pool {
+    let pools = vec![  //just reminding myself that variables in rust are immutable, make it mutable by mut,! because vec is a macro 
+        Pool {                    
             token0: String::from("ETH"),
             token1: String::from("USDC"),
             tvl_usd: 1_000_000.0,
